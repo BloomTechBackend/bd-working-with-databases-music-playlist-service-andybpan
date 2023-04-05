ead2c05e25c6e43cc87425b9d8ba6868a4429412
Andy Pan <andybpan@gmail.com>
2023-04-05 16:40:26 -0700

    Sprint13_MasteryTask1

--- src/com/amazon/ata/music/playlist/service/converters/ModelConverter.java
+++ src/com/amazon/ata/music/playlist/service/converters/ModelConverter.java
@@ -3,6 +3,8 @@ package com.amazon.ata.music.playlist.service.converters;
 import com.amazon.ata.music.playlist.service.models.PlaylistModel;
 import com.amazon.ata.music.playlist.service.dynamodb.models.Playlist;
 
+import java.util.ArrayList;
+
 public class ModelConverter {
     /**
      * Converts a provided {@link Playlist} into a {@link PlaylistModel} representation.
@@ -10,8 +12,19 @@ public class ModelConverter {
      * @return the converted playlist
      */
     public PlaylistModel toPlaylistModel(Playlist playlist) {
+        ArrayList<String> tags;
+        if (playlist.getTags() == null) {
+            tags = null;
+        } else {
+            tags = new ArrayList<>(playlist.getTags());
+        }
+
         return PlaylistModel.builder()
             .withId(playlist.getId())
+            .withName(playlist.getName())
+            .withCustomerId(playlist.getCustomerId())
+            .withSongCount(playlist.getSongCount())
+            .withTags(tags)
             .build();
     }
 }
